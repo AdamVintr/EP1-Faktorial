a2141867c8e638f4f12ac7ab28b8dbf81c173b6c
vintr.adam <vintr.adam@D25-11>
2024-05-14 08:59:46 +0200

    faktorial - update 2

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -3,16 +3,22 @@ Imports System
 Module Program
     Sub Main(args As String())
         Dim cislo As Integer
+        Dim vstup As String
         Dim soucet As Integer = 0
 
         Console.WriteLine("Zadej èíslo: ")
-        cislo = Console.ReadLine
+        vstup = Console.ReadLine
 
-        For i = 1 To cislo
-            soucet = soucet + 1
-        Next
+        If Integer.TryParse(vstup, cislo) Then
+            For i = 1 To cislo
+                soucet = soucet + 1
+            Next
 
-        Console.WriteLine($" Souèet od 1 do {cislo} je {soucet}")
+            Console.WriteLine($" Souèet od 1 do {cislo} je {soucet}")
+        Else
+            Console.WriteLine("Musíte zadat jen celá èísla!")
 
+
+        End If
     End Sub
 End Module
